5024304fda529319410fce9de0add7754b5e36ed
Miłosz <milosz.gus@gmail.com>
2019-03-01 19:52:29 +0100

    adding buttons

--- ParseStarterProject/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ ParseStarterProject/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f090030;
-        public static final int end = 0x7f090073;
-        public static final int gone = 0x7f090082;
-        public static final int invisible = 0x7f09009d;
-        public static final int left = 0x7f0900a2;
-        public static final int packed = 0x7f0900c9;
-        public static final int parent = 0x7f0900cb;
-        public static final int percent = 0x7f0900cf;
-        public static final int right = 0x7f0900e1;
-        public static final int spread = 0x7f09010b;
-        public static final int spread_inside = 0x7f09010c;
-        public static final int start = 0x7f090111;
-        public static final int top = 0x7f090132;
-        public static final int wrap = 0x7f090143;
+        public static final int end = 0x7f090077;
+        public static final int gone = 0x7f090086;
+        public static final int invisible = 0x7f0900a1;
+        public static final int left = 0x7f0900a6;
+        public static final int packed = 0x7f0900cd;
+        public static final int parent = 0x7f0900cf;
+        public static final int percent = 0x7f0900d3;
+        public static final int right = 0x7f0900e5;
+        public static final int spread = 0x7f09010f;
+        public static final int spread_inside = 0x7f090110;
+        public static final int start = 0x7f090115;
+        public static final int top = 0x7f090136;
+        public static final int wrap = 0x7f090147;
     }
     public static final class styleable {
         private styleable() {}
